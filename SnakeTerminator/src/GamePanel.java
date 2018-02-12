0e8fec5d64235c97f6162da8a813c46a11f4f07d
anissfadel123 <anissfadel123@yahoo.com>
2018-02-11 20:28:31 -0500

    designed frame

--- SnakeTerminator/src/GamePanel.java
+++ SnakeTerminator/src/GamePanel.java
@@ -1,5 +1,10 @@
+import java.awt.Color;
+
 import javax.swing.JPanel;
 public class GamePanel extends JPanel{
-	
+	GamePanel(){
+		setBackground(Color.blue);
+		setSize(700,450);
+	}
 
 }
--- SnakeTerminator/src/Main.java
+++ SnakeTerminator/src/Main.java
@@ -1,27 +1,45 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Window.Type;
 import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
 
 public class Main {
+	
 
 	public static void main(String[] args) {
+		ImageIcon title;
 		JFrame frame = new JFrame();
+		JLabel label;
 		GamePanel game = new GamePanel();
+
 		
-		frame.setTitle("Snakinator");
+		
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setBackground(Color.white);
+	
+		
 		frame.setVisible(true);
 		frame.setSize(800, 600);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//inserts the label of the title into the frame
+		title = new ImageIcon("Title.png");
+		label = new JLabel(title);
+		frame.getContentPane().add(label);
+		label.setBounds(50, 0, 700, 70);
+		
 		frame.getContentPane().add(game);
-		game.setBackground(new Color(145,0,0));
+		game.setLocation(50, 90);
 		
 		
-		//frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
+
 		
 
 	}
